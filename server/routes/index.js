233500b7efc8e9180a021b1db302eb08e57bec35
eunah3693 <https://github.com/eunah3693/w_gumanagementReact.git>
2021-11-30 16:07:24 +0900

    node.js 연동

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,21 @@
 var express = require("express");
 var router = express.Router();
+const bodyParser = require("body-parser");
+const mysql = require("mysql"); // mysql 모듈 사용
+var connection = mysql.createConnection({
+    host : "localhost",
+    user : "root", //mysql의 id
+    password : "KKoj248624!", //mysql의 password
+    database : "test", //사용할 데이터베이스
+});
 
-router.get("/",function(req,res){
-    res.send({greeting:"Hello"});
+connection.connect();
+router.get('/', (req,res) => {
+    const test = req.body.test;
+    // console.log(req.body);
+    connection.query('SELECT * from test', (error, rows) => {  //쿼리문 
+        if (error) throw error;
+        res.send("dd");
+      });
 })
-module.exports=router;
-
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const api = require("./routes/index");
 
-app.use("/api",api);
-const port=3000;
-app.listen(port,()=>console.log(`listening ${port}`));
+app.use('/api', api);
+
+const port = 3002;
+app.listen(port,()=>console.log(`Listening on port ${port}`));
+
 
